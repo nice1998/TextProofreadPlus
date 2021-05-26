12b4bb12eb41ee2c2d98af1ce825e687e7b543dc
nice1998 <active_xjp@163,com>
2021-05-26 18:02:51 +0800

    test111111111

--- src/test/java/TextProofreadPlus/AppTest.java
+++ src/test/java/TextProofreadPlus/AppTest.java
@@ -18,9 +18,9 @@ public class AppTest {
        File AlterTextFile = new File("D:\\a我的文本校对\\AlterTextFile");
 
        if (AlterTextFile.exists()){
-
+           System.out.println("文件存在");
        }else{
-           AlterTextFile.mkdir();
+           System.out.println("文件不存在");
        }
     }
 }
